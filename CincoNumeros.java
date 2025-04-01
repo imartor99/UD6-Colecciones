682211ac47487ad518233e4af26b5296788fe84b
imartor99 <imartor769@g.educaand.es>
2025-04-01 08:48:19 +0000

    Creo la clase CincoNumeros del ejercicio 4 y realizo el programa usando HashSet y for-each para suma del contenido del HashSet

---
+++ CincoNumeros.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class CincoNumeros {
+    public static void main(String[] args) {
+
+        // Definimos el HashSet
+        HashSet<Integer> numeros = new HashSet<>();
+
+        // Declaramos las variables necesarias
+        int num;
+        int suma;
+        Scanner entrada = new Scanner(System.in);
+
+        // Recogemos los 5 números introducidos por el usuario
+        for (int i = 0; i <1 5; i++) {
+            System.out.println("Introduce un número: ");
+            num = entrada.nextInt();
+            numeros.add(num);
+        }
+
+        // Calculamos la suma de los numeros contenidos en el HashSet numeros con un bucle for-each
+        suma = 0;
+        for (Integer n : numeros) {
+            suma += n;
+        }
+        
+        //Mostramos 
+        System.out.println("La suma de los números es: " + suma);
+    }
+}
