859fe797c044ed14de435c9618d9d674c32e62ef
imartor99 <imartor769@g.educaand.es>
2025-04-03 11:42:44 +0000

    depuro código MainSuper

--- SupermercadoHashMap/src/MainSuper.java
+++ SupermercadoHashMap/src/MainSuper.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public class MainSuper {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        // Defino el diccionario HashMap productos que almacenará los diferentes con sus precios.
         Map<String, Double> productos = new HashMap<>();
 
         productos.put("avena", 2.21);
@@ -16,8 +16,10 @@ public class MainSuper {
         productos.put("quinoa", 4.50);
         productos.put("guisantes", 1.60);
 
-        List<Compra> carrito = new ArrayList<>();
+        //Creo un ArrayList donde almacenaré cada Compra(objeto de la clase Compra) que realice el usuario por pantalla.
+        ArrayList<Compra> carrito = new ArrayList<>();
 
+        //Bucle con condiciones que controlen la entrada por pantalla.
         while (true) {
             System.out.print("Producto: ");
             String producto = entrada.nextLine();
@@ -36,17 +38,18 @@ public class MainSuper {
 
         }
 
+        // Imprimo el contenido del carrito y totales de compra, y formateo la salida.
         double total = 0;
         System.out.println("\n--------------------------------------");
         System.out.println("Producto   Precio   Cantidad   Subtotal");
-        System.out.println("--------------------------------------");
+        System.out.println("----------------------------------------");
         for (Compra compra : carrito) {
             double precio = productos.get(compra.getProducto());
             double subtotal = compra.getCantidad() * precio;
             total += subtotal;
             System.out.printf("%-10s %.2f     %5d       %.2f€\n", compra.getProducto(), precio, compra.getCantidad(), subtotal);
         }
-        System.out.println("--------------------------------------");
+        System.out.println("----------------------------------------");
         System.out.printf("TOTAL: %.2f€\n", total);
         entrada.close();
     }
